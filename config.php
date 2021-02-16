345bcede39c0f617e744031b305ed520a19277d0
Prouza Vojtěch <51821713+vprouza@users.noreply.github.com>
2021-02-16 19:37:31 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 const dbhost = "localhost";
-const dbuser = "prouzav";
-const dbpass = "Fact0ri0j3sup3r";
-const dbname = "6ep_prouzav";
+const dbuser = "USERNAME";
+const dbpass = "YOUR_PASSWORD";
+const dbname = "DB_NAME";
